f80b26651d535886636fca5f22040f6ec844003d
esya2208 <eseney2@gmail.com>
2025-04-24 16:59:07 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,34 +1,16 @@
 
 from aiogram import Bot, Dispatcher, types
-from aiogram.utils.executor import start_webhook
+from aiogram.utils import executor
 import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-WEBHOOK_URL = os.getenv("WEBHOOK_URL")
-WEBHOOK_PATH = "/webhook"
-WEBAPP_HOST = "0.0.0.0"
-WEBAPP_PORT = int(os.getenv("PORT", 8080))
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
 
 @dp.message_handler(commands=["start"])
 async def start_handler(message: types.Message):
-    await message.answer("👋 Привет! Бот запущен на Railway с Webhook!")
-
-async def on_startup(dp):
-    await bot.set_webhook(WEBHOOK_URL + WEBHOOK_PATH)
-
-async def on_shutdown(dp):
-    await bot.delete_webhook()
+    await message.answer("👋 Привет! Бот работает на Render через polling!")
 
 if __name__ == "__main__":
-    start_webhook(
-        dispatcher=dp,
-        webhook_path=WEBHOOK_PATH,
-        on_startup=on_startup,
-        on_shutdown=on_shutdown,
-        skip_updates=True,
-        host=WEBAPP_HOST,
-        port=WEBAPP_PORT
-    )
+    executor.start_polling(dp, skip_updates=True)
